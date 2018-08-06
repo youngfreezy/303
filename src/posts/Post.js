2577f546c4c873b45d1a472c5f9ed44dd7031025
Fareez Ahmed <fareezahmed@Fareezs-MacBook-Pro.local>
2018-08-06 19:40:41 -0400

    styling

--- src/posts/Post.js
+++ src/posts/Post.js
@@ -2,13 +2,13 @@ import React, { PureComponent } from "react";
 import classNames from "classnames";
 export default class Post extends PureComponent {
   render() {
-  	const postClasses = classNames({
+  	const postClasses = classNames("post", {
   		published: this.props.status === "publish"
   	});
     
     return <div className="col">
-    	<div dangerouslySetInnerHTML={{ __html: this.props.excerpt }}></div>
-    	<div className={postClasses}> 
+    	<div className={postClasses} dangerouslySetInnerHTML={{ __html: this.props.excerpt }}></div>
+    	<div > 
     		<a href={this.props.link} target="_blank">{this.props.title}</a> 
     	</div> 
     </div>
